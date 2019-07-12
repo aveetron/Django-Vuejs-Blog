9533857ff623e0e60c509851db371b296bc40119
avee <aveechakra@gmail.com>
2019-07-12 08:59:55 +0600

    api relations added

--- BlogBackend/blog/admin.py
+++ BlogBackend/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+# Register your models here for showing this model to admin site
+admin.site.register(Blog)
--- BlogBackend/blog/models.py
+++ BlogBackend/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Blog model decribing it's properties
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
